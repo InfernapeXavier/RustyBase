f859ad1c4a735b945bf35507a95b522a15e3009d
InfernapeXavier <itsgood37@ymail.com>
2020-04-24 21:28:20 -0400

    Clean Main

--- src/main.rs
+++ src/main.rs
@@ -26,94 +26,8 @@ mod schema;
 lalrpop_mod!(pub parser);
 
 fn main() {
-    println!("\n\nExecuting Main.........");
-    // print!("\n\nEnter in your CNF: ");
-    // io::stdout().flush().unwrap();
-    // let mut input = String::new();
-    // io::stdin()
-    //     .read_line(&mut input)
-    //     .expect("Can't parse your CNF");
-    // input = input.trim().to_string();
-    // println!("\n\nYou entered: {}", input);
-
-    // // Creating Schema
-    // let mut my_schema = schema::Schema::new();
-    // let catalog = Path::new("src/scratch/catalog");
-    // my_schema = my_schema.build(catalog, "nation");
-    // let my_comparison = comparison::CNF::new();
-    // my_comparison.grow_from_parse_tree();
+    println!("\n\nExecuted Main.........");
 }
-
-// #[test]
-// fn test1() {
-//     // Schema+Record Test
-//     let mut my_schema = schema::Schema::new();
-
-//     let catalog = Path::new("src/scratch/catalog");
-//     my_schema = my_schema.build(catalog, "nation");
-
-//     let record_file = Path::new("src/scratch/nation.tbl");
-
-//     let mut my_record = record::Record::new(record_file);
-//     my_record.suck_next_record(&my_schema);
-//     my_record.print(&my_schema);
-//     my_record.suck_next_record(&my_schema);
-//     my_record.print(&my_schema);
-
-//     // // Comparison Test
-//     let comparison = comparison::Comparison::new(
-//         defs::Target::Left,
-//         defs::Target::Right,
-//         4,
-//         5,
-//         defs::DataType::INT,
-//         defs::CompOperator::LessThan,
-//     );
-//     comparison.print();
-// }
-
-// #[test]
-// fn test2() {
-//     let parse_tree = parser::ParseTreeParser::new()
-//         .parse("(l_orderkey > 27) AND (l_orderkey < 45)")
-//         .unwrap();
-//     println!("{:#?}\n\n", parse_tree);
-//     // Creating Schema
-//     let mut my_schema = schema::Schema::new();
-//     let catalog = Path::new("src/scratch/catalog");
-//     my_schema = my_schema.build(catalog, "lineitem");
-//     let out_rec_file = File::create("sdafdsfFFDSDA").expect("Could not create record file");
-//     let out_rec_path = Path::new("sdafdsfFFDSDA");
-//     let mut literal = record::Record::new(out_rec_path);
-//     let mut my_comparison = comparison::CNF::new();
-//     my_comparison =
-//         my_comparison.grow_from_parse_tree(parse_tree, &out_rec_file, &my_schema, &mut literal);
-//     my_comparison.print();
-//     my_comparison.display();
-// }
-
-// #[test]
-// fn lalrtest() {
-//     let expression = parser::ParseTreeParser::new()
-//         .parse("(l_orderkey > 27) AND (l_orderkey < 'don't')")
-//         .unwrap();
-//     println!("{:#?}", expression);
-// }
-
-// #[test]
-// fn record_test() {
-//     let mut my_schema = schema::Schema::new();
-
-//     let catalog = Path::new("src/scratch/catalog");
-//     my_schema = my_schema.build(catalog, "nation");
-//     let path = Path::new("src/scratch/nation.tbl");
-
-//     let mut my_record = record::Record::new(path);
-//     my_record.suck_next_record(&my_schema);
-//     my_record.print(&my_schema);
-//     my_record.suck_next_record(&my_schema);
-//     my_record.print(&my_schema);
-// }
 
 #[test]
 fn main_test() {
@@ -144,8 +58,6 @@ fn main_test() {
     let mut my_comparison = comparison::CNF::new();
     my_comparison =
         my_comparison.grow_from_parse_tree(expression, &out_rec_file, &lineitem, &mut literal);
-    // my_comparison.print();
-    // my_comparison.display();
 
     // Building the temp record
     let table_file = Path::new("src/tpch/lineitem.tbl");

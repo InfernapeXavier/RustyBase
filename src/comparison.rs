fa83553305d738345722e6c180dda0bdf190dc8b
InfernapeXavier <itsgood37@ymail.com>
2020-04-27 12:05:59 -0400

    FInished Comparison
    - Requires some testing
    - Rust doesn't have overloading so every function has a different name

--- src/comparison.rs
+++ src/comparison.rs
@@ -192,9 +192,242 @@ impl CNF {
         }
     }
 
+    pub fn grow_from_parse_tree_two(
+        mut self,
+        mut parse_tree: AndList,
+        out_rec_file: &File,
+        left_schema: &Schema,
+        right_schema: &Schema,
+        literal: &mut Record,
+    ) -> CNF {
+        // Building up the literal and schema in an external text file to read from later
+        let mut out_schema_file =
+            File::create("hkljdfgkSDFSDF").expect("Could not create schema file");
+        out_schema_file
+            .write_all(b"BEGIN\ntempSchema\nwherever\n")
+            .expect("Can't write to schema file");
+
+        // Tracks the size of literal record
+        let mut num_fields_in_lit: i64 = 0;
+
+        // Building the comparison structure
+        let mut which_and = 0;
+
+        loop {
+            let mut comparisons: Vec<Comparison> = Vec::new();
+            let mut my_or: OrList = *parse_tree.left;
+            let mut which_or = 0;
+            loop {
+                let type_left: DataType;
+                let type_right: DataType;
+
+                // Required for building the Comparison structure locally
+                let operand_one: Target;
+                let operand_two: Target;
+                let which_att_one: i64;
+                let which_att_two: i64;
+                let att_type: DataType;
+                let op: CompOperator;
+
+                // dealing with the left operand
+                match my_or.left.left.code {
+                    parsedefs::NAME => {
+                        // Try to find the attribute in the left schema
+                        if left_schema.find(&my_or.left.left.value) != 1 {
+                            operand_one = Target::Left;
+                            which_att_one = left_schema.find(&my_or.left.left.value);
+                            type_left = left_schema.find_type(&my_or.left.left.value);
+                        } else
+                        //Try to find in the right schema
+                        if left_schema.find(&my_or.left.left.value) != 1 {
+                            operand_one = Target::Right;
+                            which_att_one = right_schema.find(&my_or.left.left.value);
+                            type_left = right_schema.find_type(&my_or.left.left.value);
+                        } else {
+                            panic!(
+                                "ERROR: Could not find attribute {} \n",
+                                my_or.left.left.value
+                            );
+                        }
+                    }
+                    parsedefs::STRING => {
+                        // It it's a string then add to the literal record
+                        operand_one = Target::Literal;
+                        which_att_one = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.left.value,
+                            DataType::STRING,
+                        );
+                        type_left = DataType::STRING;
+                    }
+
+                    parsedefs::INT => {
+                        // Same for Int
+                        operand_one = Target::Literal;
+                        which_att_one = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.left.value,
+                            DataType::INT,
+                        );
+                        type_left = DataType::INT;
+                    }
+
+                    parsedefs::DOUBLE => {
+                        // Same for Int
+                        operand_one = Target::Literal;
+                        which_att_one = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.left.value,
+                            DataType::DOUBLE,
+                        );
+                        type_left = DataType::DOUBLE;
+                    }
+
+                    _ => panic!("Unrecognized operand type"),
+                }
+
+                // Dealing with the right operand
+                match my_or.left.right.code {
+                    parsedefs::NAME => {
+                        // If it's an attribute name, look it up in the left schema
+                        if left_schema.find(&my_or.left.right.value) != 1 {
+                            operand_two = Target::Left;
+                            which_att_two = left_schema.find(&my_or.left.right.value);
+                            type_right = left_schema.find_type(&my_or.left.right.value);
+                        } else
+                        // Check if it's in the right schema
+                        if right_schema.find(&my_or.left.right.value) != 1 {
+                            operand_two = Target::Right;
+                            which_att_two = left_schema.find(&my_or.left.right.value);
+                            type_right = left_schema.find_type(&my_or.left.right.value);
+                        } else {
+                            // There's an error in the query
+                            panic!(
+                                "ERROR: Could not find attribute {} \n",
+                                my_or.left.right.value
+                            );
+                        }
+                    }
+                    parsedefs::STRING => {
+                        // It it's a string then add to the literal record
+                        operand_two = Target::Literal;
+                        which_att_two = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.right.value,
+                            DataType::STRING,
+                        );
+                        type_right = DataType::STRING;
+                    }
+
+                    parsedefs::INT => {
+                        // Same for Int
+                        operand_two = Target::Literal;
+                        which_att_two = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.right.value,
+                            DataType::INT,
+                        );
+                        type_right = DataType::INT;
+                    }
+
+                    parsedefs::DOUBLE => {
+                        // Same for Int
+                        operand_two = Target::Literal;
+                        which_att_two = num_fields_in_lit;
+                        num_fields_in_lit = add_lit_to_file(
+                            num_fields_in_lit,
+                            &out_rec_file,
+                            &out_schema_file,
+                            &my_or.left.right.value,
+                            DataType::DOUBLE,
+                        );
+                        type_right = DataType::DOUBLE;
+                    }
+
+                    _ => panic!("Unrecognized operand type"),
+                }
+
+                // End of Match
+                // Now we check for type mismatch between left and right operand
+
+                if type_left != type_right {
+                    panic!(
+                        "ERROR: Type mismatch in CNF. {} and {} don't match.\n",
+                        &my_or.left.left.value, &my_or.left.right.value
+                    )
+                }
+                // Set attribute type for the comparison
+                att_type = type_right;
+
+                // Setting up the comparison operator
+                match my_or.left.code {
+                    parsedefs::LESS_THAN => op = CompOperator::LessThan,
+                    parsedefs::GREATER_THAN => op = CompOperator::GreaterThan,
+                    parsedefs::EQUALS => op = CompOperator::Equals,
+                    _ => panic!("BAD: Found a comparison operator that is not recognized"),
+                }
+
+                let temp_comparison = Comparison::new(
+                    operand_one,
+                    operand_two,
+                    which_att_one,
+                    which_att_two,
+                    att_type,
+                    op,
+                );
+                comparisons.push(temp_comparison);
+
+                which_or = which_or + 1;
+                my_or = match my_or.right_or {
+                    None => {
+                        self.or_lens.push(which_or);
+                        break;
+                    }
+                    Some(x) => *x,
+                }
+            }
+            self.or_list.push(comparisons);
+            which_and = which_and + 1;
+            parse_tree = match parse_tree.right_and {
+                None => {
+                    self.num_ands = which_and;
+                    break;
+                }
+                Some(x) => *x,
+            }
+        }
+
+        out_schema_file
+            .write_all(b"END\n")
+            .expect("Can't write to schema file");
+        let out_schema_path = Path::new("hkljdfgkSDFSDF");
+        let out_rec_path = Path::new("sdafdsfFFDSDA");
+        let mut out_schema: Schema = Schema::new();
+        out_schema = out_schema.build(out_schema_path, "tempSchema");
+        literal.suck_next_record(&out_schema);
+        fs::remove_file(out_schema_path).expect("Failed to remove temporary schema file");
+        fs::remove_file(out_rec_path).expect("Failed to remove temporary record file");
+        self
+    }
+
     // This takes a parse tree for a CNF and converts it into a 2-D matrix
     // This version is applicable to selections over a single relation
-    pub fn grow_from_parse_tree(
+    pub fn grow_from_parse_tree_single(
         mut self,
         mut parse_tree: AndList,
         out_rec_file: &File,
@@ -292,7 +525,7 @@ impl CNF {
                 // Dealing with the right operand
                 match my_or.left.right.code {
                     parsedefs::NAME => {
-                        // If it's an attribute name, look it up in the left schema
+                        // If it's an attribute name, look it up in the schema
                         if my_schema.find(&my_or.left.right.value) != 1 {
                             operand_two = Target::Left;
                             which_att_two = my_schema.find(&my_or.left.right.value);
@@ -478,6 +711,7 @@ impl CNF {
     }
 }
 
+// This is a helper function used by CNF
 fn add_lit_to_file(
     num_fields_in_lit: i64,
     mut out_rec_file: &File,
